52630c03bc4fcb462b302eebbbd6cf86bfa3cf1a
Ernesto R. C. Pereda <ercp42@gmail.com>
2023-01-24 20:56:17 -0500

    2022 Day 3 part2

--- 2022/day3.py
+++ 2022/day3.py
@@ -21,16 +21,39 @@ def compute_incorrect_item_priority(rucksack: str) -> int:
     return get_priority(item)
 
 
+def compute_rucksack_repeted_item(r1: str, r2: str, r3: str):
+    s1 = set(r1)
+    s2 = set(r2)
+    s3 = set(r3)
+
+    return (s1 & s2 & s3).pop()
+
+
 def main():
-    input = sys.argv[1]
+    part = sys.argv[1]
+    input = sys.argv[2]
 
     with open(input) as file:
-        print(
-            sum(
-                compute_incorrect_item_priority(line.strip())
-                for line in file.readlines()
+        if part == "1":
+            print(
+                sum(
+                    compute_incorrect_item_priority(line.strip())
+                    for line in file.readlines()
+                )
             )
-        )
+            return
+        elif part == "2":
+            total = 0
+            while True:
+                r1 = file.readline().strip()
+                r2 = file.readline().strip()
+                r3 = file.readline().strip()
+                if not (r1 and r2 and r3):
+                    break
+                item = compute_rucksack_repeted_item(r1, r2, r3)
+                priority = get_priority(item)
+                total += priority
+            print(total)
 
 
 if __name__ == "__main__":
