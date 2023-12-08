722c2ea21271af6f75e1df52b8c1e0f8f18c3cf7
Ernesto R. C. Pereda <ercp42@gmail.com>
2023-12-08 01:52:21 -0500

    2023 Day6

---
+++ 2023/ocaml/bin/day6.ml
@@ -0,0 +1,9 @@
+(* open Base *)
+
+let () =
+  Stdio.print_endline "=== Part 1 ===";
+  Stdio.print_endline "Not implemented"
+
+let () =
+  Stdio.print_endline "=== Part 2 ===";
+  Stdio.print_endline "Not implemented"
